7bc33e3ebcb21ec63373022ac123eb8fce854601
متدرب5 - - <Training5@ext.ntis.com.sa>
2026-09-21 14:52:01 +0300

    Add cors

--- src/Gateway/ApiGateway/Program.cs
+++ src/Gateway/ApiGateway/Program.cs
@@ -4,10 +4,21 @@ builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "API Gateway is running");
-
+app.UseCors("AllowAll");
 app.MapReverseProxy();
 
 app.Run();
